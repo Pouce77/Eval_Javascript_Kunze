fccaa10b89fabc80149d7f69450ce82c91c8cdae
Julien Kunze <julienkunze0@gmail.com>
2022-08-27 14:45:42 +0200

    premier commit

--- js/dice.js
+++ js/dice.js
@@ -8,12 +8,14 @@ class Player{
   }
   
 }
-//Evénement du click sur le bouton 'roll'
+//Initialisation des variables
 var tour=1;
 var playerOne=new Player("score1","global1",0,0);
 var playerTwo=new Player("score2","global2",0,0);
 var roll=document.getElementById("roll");
+var hold=document.getElementById("hold");
 
+//Evénement du click sur le bouton 'roll'
 roll.addEventListener('click',()=>{
   var dice=document.getElementById("nbDice");
   var tabChiffre=["img/un.png","img/deux.png","img/trois.png","img/quatre.png","img/cinq.png","img/six.png"];
@@ -35,32 +37,73 @@ roll.addEventListener('click',()=>{
         var chiffre=entierAleatoire(1,6);
         dice.setAttribute("src",tabChiffre[chiffre-1]);
         console.log('le chiffre est '+ tabChiffre[chiffre-1]);
+        var current=document.getElementById(player.currentScoreName);
+
+        // si le résultat est 1
         if(chiffre==1){
+          current.innerHTML=0;
+
+          //si c'est le tour du joueur 1
           if(tour==1){
+            changeTour(tour);
             tour=2;
+          //si c'est le tour du joueur 2
           }else{
+            changeTour(tour);
             tour=1;
           }
         }else{
-          
         //changement du current score du joueur en cours
-        var current=document.getElementById(player.currentScoreName);
         var score=parseInt(current.innerHTML);
         score=score+chiffre;
         current.innerHTML=score;
         console.log(score);
+        player.current=score;
         }
       }
-    },100);
-
-  
-  
-    
-  
+    },100);  
 });
 
+hold.addEventListener('click',()=>{
+
+  if(tour==1){
+    playerOne.global+=playerOne.current;
+    changeGlobal(playerOne.globalScoreName,playerOne.currentScoreName,playerOne.global);
+    changeTour(1);
+    tour=2;
+    playerOne.current=0;
+  }else{
+    playerTwo.global+=playerTwo.current;
+    changeGlobal(playerTwo.globalScoreName,playerTwo.currentScoreName,playerTwo.global);
+    changeTour(2);
+    tour=1;
+    playerTwo.current=0;
+  }
+
+});
 
 function entierAleatoire(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function changeGlobal(id,idCurrent,score){
+  var global=document.getElementById(id);
+  var current=document.getElementById(idCurrent);
+  global.innerHTML=score;
+  current.innerHTML=0;
+}
+
+function changeTour(tour){
+  if(tour==1){
+    var circle1=document.getElementById("circle1");
+    var circle2=document.getElementById("circle2");
+    circle1.style.display="none";
+    circle2.style.display="initial"
+  
+  }else{
+    var circle1=document.getElementById("circle1");
+    var circle2=document.getElementById("circle2");
+    circle1.style.display="initial";
+    circle2.style.display="none";
+  }
+}
