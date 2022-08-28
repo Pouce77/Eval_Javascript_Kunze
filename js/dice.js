95d4483b410950c8d63257a0942383c323dd8e61
Julien Kunze <julienkunze0@gmail.com>
2022-08-28 17:21:17 +0200

    correction score à100

--- js/dice.js
+++ js/dice.js
@@ -75,7 +75,7 @@ hold.addEventListener('click',()=>{
 
   if(tour==1){
     playerOne.global+=playerOne.current;
-      if(playerOne.global>=20){
+      if(playerOne.global>=100){
         var end=document.getElementById('end');
         end.style.display="initial";
         changeGlobal(playerOne.globalScoreName,playerOne.currentScoreName,playerOne.global);
